db91b493c9e1079dd184eaaf7ecf8014b75f554f
chris <chris.snake@gmx.net>
2017-05-26 22:23:11 +0200

    init calc-db (backend)

---
+++ calc-db/src/main/java/com/example/calc/CalcDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcDbApplication.class, args);
+	}
+}
