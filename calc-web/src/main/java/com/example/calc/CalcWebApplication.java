4a5c90db9b9f12859a94fb1dace362b7d09edf10
Rene <zaidor@hotmail.de>
2017-05-26 20:19:08 +0200

    init calc-web

---
+++ calc-web/src/main/java/com/example/calc/CalcWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcWebApplication.class, args);
+	}
+}
