e7ac73f04fd521a57ffa0f49a1ece986127929b6
chris <chris.snake@gmx.net>
2017-05-30 23:31:35 +0200

    Use H2 for test cases

--- calc-db/src/test/java/com/example/calc/CalcDbApplicationTests.java
+++ calc-db/src/test/java/com/example/calc/CalcDbApplicationTests.java
@@ -2,11 +2,13 @@ package com.example.calc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class CalcDbApplicationTests {
 
 	@Test
