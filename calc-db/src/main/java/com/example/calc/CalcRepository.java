3309ff1822ca5974de7f291bd6d1c930d710e641
chris <chris.snake@gmx.net>
2017-05-30 22:38:18 +0200

    Changed .travis.yml for mysql

--- calc-db/src/main/java/com/example/calc/CalcRepository.java
+++ calc-db/src/main/java/com/example/calc/CalcRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 @Transactional
 public interface CalcRepository extends CrudRepository<CalcEntry, Long> {
 	
+	// TODO: eigene Query implementieren, sh. 
+	// https://github.com/netgloo/spring-boot-samples/blob/master/spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/models/UserDao.java
+	
 }
