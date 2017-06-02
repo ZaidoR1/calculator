987a491d929a782f6ad956f08b0bca1386a1730e
Rene <zaidor@hotmail.de>
2017-06-02 22:17:43 +0200

    close #10 changed calculation enitity and much other

--- calc-db/src/main/java/com/example/calc/CalcController.java
+++ calc-db/src/main/java/com/example/calc/CalcController.java
@@ -1,90 +1,60 @@
 package com.example.calc;
 
-import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcController {
-	
-	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass()); 
-	
+
 	@Autowired
 	private CalcRepository repo;
 
 	@GetMapping("histories")
 	public ResponseEntity<?> getAll() {
-		return ResponseEntity.ok(repo.findAll());
+		return ResponseEntity.ok(repo.findAllByOrderByIdDesc());
 	}
-	
+
 	@GetMapping("history/{id}")
 	public ResponseEntity<?> get(@PathVariable Long id) {
-		CalcEntry entry = repo.findOne(id);
+		final CalcEntry entry = repo.findOne(id);
 		if (entry != null) {
 			return ResponseEntity.ok(entry);
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping("history")
 	public ResponseEntity<?> add(@RequestBody CalcEntry entry) {
-		
-		try {
-			entry = CalcWorker.getResult( entry );
-		} catch (Exception e) {
-			LOGGER.error("Exception while calculation result for {}", entry.getOperation());
-			LOGGER.error("Stacktrace: ", e);
-			
-			// TODO: Error Message (not only Error Status) to Frontend?
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-		
 		return ResponseEntity.ok(repo.save(entry));
 	}
-	
-	@PutMapping("history/{id}")
-	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody CalcEntry entry) {
-		CalcEntry oldEntry = repo.findOne(id);
-		if (oldEntry != null) {
-			try {
-				entry = CalcWorker.getResult( entry );
-			} catch (Exception e) {
-				LOGGER.error("Exception while calculation result for {}", entry.getOperation());
-				LOGGER.error("Stacktrace: ", e);
-				
-				// TODO: Error Message (not only Error Status) to Frontend?
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-			}
-			
-			oldEntry.setOperation(entry.getOperation());
-			oldEntry.setResult(entry.getResult());
-			oldEntry.setDate(new Date());
-			return ResponseEntity.ok(repo.save(oldEntry));
-		}
-		
-		LOGGER.info("Entry with id {} not found for updating", id);
-		return ResponseEntity.notFound().build();
-	}
-	
-	@DeleteMapping("history/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {
-		try {
-			repo.delete(repo.findOne(id));
-			return ResponseEntity.ok().build();
-		} catch (Exception e) {
-			LOGGER.info("Entry with id {} not found for deleting", id);
-			return ResponseEntity.notFound().build();
-		}
-	}
+
+	// @PutMapping("history/{id}")
+	// public ResponseEntity<?> update(@PathVariable Long id, @RequestBody
+	// CalcEntry entry) {
+	// CalcEntry oldEntry = repo.findOne(id);
+	// if (oldEntry != null) {
+	// oldEntry.setOperation(entry.getOperation());
+	// oldEntry.setDate(new Date());
+	// return ResponseEntity.ok(repo.save(oldEntry));
+	// }
+	//
+	// LOGGER.info("Entry with id {} not found for updating", id);
+	// return ResponseEntity.notFound().build();
+	// }
+
+	// @DeleteMapping("history/{id}")
+	// public ResponseEntity<?> delete(@PathVariable Long id) {
+	// try {
+	// repo.delete(repo.findOne(id));
+	// return ResponseEntity.ok().build();
+	// } catch (Exception e) {
+	// LOGGER.info("Entry with id {} not found for deleting", id);
+	// return ResponseEntity.notFound().build();
+	// }
+	// }
 }
--- calc-db/src/main/java/com/example/calc/CalcWorker.java
+++ calc-db/src/main/java/com/example/calc/CalcWorker.java
@@ -1,39 +1,22 @@
 package com.example.calc;
 
 import com.udojava.evalex.Expression;
-import com.udojava.evalex.Expression.ExpressionException;
 
-public class CalcWorker
-{
+public class CalcWorker {
 
 	/**
-	 * Calculates a result, based on the operation string entry.operation
-	 * 
-	 * Operation may be something like "3+5*7+17" or "SQRT(9)" or "100.678/2^3" or "(3+5)*2"
-	 * (see {@linkplain}https://github.com/uklimaschewski/EvalEx )
+	 * Calculates a result, based on the operation string entry. Operation may
+	 * be something like "3+5*7+17" or "SQRT(9)" or "100.678/2^3" or "(3+5)*2"
+	 * or whatever
 	 * 
 	 * @param entry
-	 * @return entry with calculated result
+	 * @return double the result
 	 * @throws Exception
-	 * 
+	 *             if e.g. invalid operation
+	 * @see <a href=
+	 *      "https://github.com/uklimaschewski/EvalEx">https://github.com/uklimaschewski/EvalEx</a>
 	 */
-	static CalcEntry getResult( CalcEntry entry ) throws Exception 
-	{
-		double result = 0.0;
-		entry.setResult( 0.0 );
-		
-		if ( entry == null || entry.getOperation() == null || entry.getOperation().length() < 1 )
-			throw new Exception( "Operation not valid" );
-		
-		try	{
-			result = new Expression( entry.getOperation()).eval().doubleValue();
-		} catch( ExpressionException e)	{
-			throw new Exception( "Operation not valid" );
-		} catch (Exception e)	{
-			throw new Exception();
-		}
-		entry.setResult( result );
-		
-		return entry;
+	static double calculate(String operation) throws Exception {
+		return new Expression(operation).eval().doubleValue();
 	}
 }
